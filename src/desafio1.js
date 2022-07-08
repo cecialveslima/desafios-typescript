c53d2b73bb1f040caf38219d6fecbd527e1690f1
cecialveslima <cecialveslima@gmail.com>
2022-07-08 16:58:08 -0300

    first commit

---
+++ src/desafio1.js
@@ -0,0 +1,10 @@
+"use strict";
+let employee;
+let funcionario = {
+    codigo: 50,
+    nome: "Joao"
+};
+let func = {
+    codigo: 98,
+    nome: "Jonas"
+};
---
+++ src/desafio1.ts
@@ -0,0 +1,20 @@
+let employee : { 
+    code: 10,
+    name: "Pedro"
+};
+
+let funcionario : {codigo:number, nome:string} = {
+    codigo: 50,
+    nome:"Joao"
+}
+
+
+interface Funcionar {
+    codigo: number,
+    nome: string
+}
+
+let func : Funcionar = {
+    codigo: 98,
+    nome: "Jonas"
+}
